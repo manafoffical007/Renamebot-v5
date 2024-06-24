dd35a02bfb0d098c31675d659a7e5e01a8366882
MANAF OFFICIAL <114959852+manafoffical007@users.noreply.github.com>
2024-06-24 16:27:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,22 +5,22 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID    = os.environ.get("API_ID", "12618934")
+    API_HASH  = os.environ.get("API_HASH", "49aacd0bc2f8924add29fb02e20c8a16")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6787206224:AAGuxnEDSwOp6CZdMNo_YqtpTiONW6K39VY") 
    
     # database config
-    DB_NAME = os.environ.get("DB_NAME","")     
-    DB_URL  = os.environ.get("DB_URL","")
+    DB_NAME = os.environ.get("DB_NAME","renamebotv3")     
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://renamebotv3:renamebotv3@cluster0.cxxhmsy.mongodb.net/")
  
     # other configs
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "")
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1297128957').split()]
 
     # channels logs
     FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002116152617"))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
